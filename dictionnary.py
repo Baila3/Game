13c8cdf37e436fa32bd6e8c78da7e9616c5251e7
Baila3 <bailaly16@gmail.com>
2023-02-18 20:42:32 -0500

    create matrix function

---
+++ dictionnary.py
@@ -0,0 +1,11 @@
+def matrixsolver():
+  matrix = []
+  rows = input("Number of rows: ")
+  cols = input("Number of columns: ")
+  i = 0
+  while i < int(rows):
+    matrix.append([])
+    i+= 1
+  print(matrix)
+
+matrixsolver()
