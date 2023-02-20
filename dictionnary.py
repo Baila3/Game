b3adeb20f032b085aceccc143c30a4d4c895d9cf
Baila3 <bailaly16@gmail.com>
2023-02-19 21:13:33 -0500

    create rows

--- dictionnary.py
+++ dictionnary.py
@@ -2,10 +2,15 @@ def matrixsolver():
   matrix = []
   rows = input("Number of rows: ")
   cols = input("Number of columns: ")
-  i = 0
-  while i < int(rows):
-    matrix.append([])
-    i+= 1
+  x = 1
+  for entries in range(int(rows) * int(cols)):
+      num = input('Enter number {0}: '.format(x))
+      matrix.append(num)
+      x+=1
+  # i = 0
+  # while i < int(rows):
+  #   matrix.append([])
+  #   i+= 1
   print(matrix)
 
 matrixsolver()
