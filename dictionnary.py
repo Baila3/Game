ebf9aa5c6769604e09850290a340f9b111dc08ea
Baila3 <bailaly16@gmail.com>
2023-02-21 23:20:09 -0500

    create seperate rows within matrix

--- dictionnary.py
+++ dictionnary.py
@@ -1,16 +1,25 @@
 def matrixsolver():
   matrix = []
+  row = []
+  rawNum = []
   rows = input("Number of rows: ")
   cols = input("Number of columns: ")
   x = 1
   for entries in range(int(rows) * int(cols)):
-      num = input('Enter number {0}: '.format(x))
-      matrix.append(num)
-      x+=1
+    num = input('Enter number {0}: '.format(x))
+    rawNum.append(num)
+    x+=1
+    row.append(num)
+  matrix = [rawNum[x:x+int(cols)] for x in range(0, len(rawNum), int(cols))]
+
+    # if len(row) > int(cols):
+    #   for entry in range(int(cols)):
+    #     matrix.append(row)  
   # i = 0
   # while i < int(rows):
   #   matrix.append([])
   #   i+= 1
   print(matrix)
+  # print(matrix)
 
 matrixsolver()
